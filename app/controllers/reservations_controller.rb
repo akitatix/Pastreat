a6af104d869a955c6b482df8c0983caa13afc713
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-03-04 17:08:34 +0100

    Basic vue and all routes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,29 @@
 class ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all
   end
 
   def new
+    @boulangerie = Boulangerie.find(params[:boulangery_id])
+    @reservation = Reservation.new
   end
 
   def create
+    @boulangerie = Boulangerie.find(params[:boulangery_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    @reservation.boulangerie = @boulangerie
+    @reservation.save
+
+    if @reservation.save
+      redirect_to reservations_path
+    else
+      render :new
+    end
+
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :price, :nb_pc, :nb_cr)
   end
 end

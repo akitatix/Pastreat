1e7582cc76e144444a77482d3a153e1145c67c79
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-03-05 23:47:45 +0100

    Toggle cross and burger need fix

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,10 +2,15 @@ console.log('gfdfds');
 
 let togg1 = document.getElementById("main-navbar-button");
 let d1 = document.getElementById("main-navbar-menu");
+let burger = document.getElementById("navbar-img1");
+let cross = document.getElementById("navbar-img2");
 
 let togg2 = document.getElementById("togg2");
 let d2 = document.getElementById("d2");
 
 togg1.addEventListener("click", () => {
   d1.classList.toggle("open");
+  console.log("je suis la");
+  burger.classList.toggle("off");
+  cross.classList.toggle("one");
 });

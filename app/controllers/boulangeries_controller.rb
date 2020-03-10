4a6da4998a84d22d06c9c959939416683ded4e7c
Paul-Antoine <gast.paulantoine@gmail.com>
2020-03-10 17:43:33 +0100

    Formulaire amélioré et corrigé

--- app/controllers/boulangeries_controller.rb
+++ app/controllers/boulangeries_controller.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'open-uri'
 class BoulangeriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :toto]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
   def index
     @boulangeries = Boulangerie.all

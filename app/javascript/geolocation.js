7243b4d4f7944e6e9e2c5954582f485c360c60a8
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-03-11 11:53:41 +0100

    Home reservation link done

--- app/javascript/geolocation.js
+++ app/javascript/geolocation.js
@@ -28,11 +28,13 @@ if (document.querySelector('[data-geolocation]')) {
                       console.log("---------------------------")
                       console.log(blabla.name)
                       console.log(blabla.badge)
+                      console.log(blabla.id)
                       console.log(counter)
                       console.log(document.querySelectorAll('.name')[counter])
                       document.querySelectorAll('.name')[counter].innerHTML = `${blabla.name}`
                       document.querySelectorAll('.fin')[counter].innerHTML = `${blabla.dist}`
                       document.querySelectorAll('[fino]')[counter].href = `https://www.google.com/maps/dir/?api=1&origin=${position.coords.latitude},${position.coords.longitude}&destination=${blabla["position"]}&travelmode=walking`;
+                      document.querySelectorAll('[reserv]')[counter].href = `/boulangeries/${blabla.id}/reservations/new`
                       if ( blabla.badge === "Or" ) {
                         document.querySelectorAll('.badge')[counter].innerHTML = `<span class="iconify" data-icon="vaadin:medal" data-inline="false" style="color: gold;"></span>`
                       } if (blabla.badge === "Argent") {

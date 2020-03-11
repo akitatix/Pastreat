2ad542a6a77c8748ba927d2b9cb76708deee3e83
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-03-11 10:52:00 +0100

    minimal error

--- app/controllers/boulangeries_controller.rb
+++ app/controllers/boulangeries_controller.rb
@@ -1,7 +1,7 @@
 require 'json'
 require 'open-uri'
 class BoulangeriesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :toto]
+  skip_before_action :authenticate_user!, only: [:index, :toto, :indexGeo]
 
   def index
     @boulangeries = Boulangerie.all

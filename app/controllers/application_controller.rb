6f0bc570249119263eb4deba9a8dd582ee35994a
Vincent Gonnard <vincent.gonnard@gmail.com>
2020-03-11 12:04:14 +0100

    meta adding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location])
   end
+
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 end

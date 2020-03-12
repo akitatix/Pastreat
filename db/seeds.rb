55f0ebecb0e3a91c5eb98160bb5eeb504a04c9b5
Yoaki Niscoise <yoakiniscoise@gmail.com>
2020-03-12 19:12:26 +0100

    bug fixxx

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
+p "Deleting all the content "
 Boulangerie.destroy_all
+User.destroy_all
+Rating.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,11 +9,69 @@ Boulangerie.destroy_all
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "Create User "
+user = User.new
+user.first_name = "Yoaki"
+user.last_name = "Niscoise"
+user.email = "yoakiniscoise@gmail.com"
+user.password = "nonnon"
+user.password_confirmation = "nonnon"
+user.save!
+Boulangerie.create!(name: 'Eric Kayser', position: "45.768540,4.836213", badge: "Or", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: 'erickayser.jpg', email:"vincent.gonnard@gmail.com")
+Boulangerie.create({ name: 'Maison Jacquard', position: "45.768947,4.832261", badge: "Or", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
+Boulangerie.create({ name: 'O tao bom', position: "45.769913,4.836036", badge: "Argent", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
+Boulangerie.create({ name: 'Antoinette Pain & Brioche', position: "45.767865,4.830878", badge: "Argent", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
+Boulangerie.create({ name: 'Au fournil du 6ème', position: "45.769465,4.844340", badge: "Bronze", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
+Boulangerie.create!(name: 'Boulangerie Cartellier', position: "45.773405, 4.845099", badge: "Or", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: 'erickayser.jpg', email:"vincent.gonnard@gmail.com")
+Boulangerie.create({ name: 'Max Poilane', position: "45.766887, 4.844491", badge: "Or", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 2, 2), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
+Boulangerie.create({ name: 'Partisan Boulanger', position: "45.777579, 4.832430", badge: "Argent", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
+Boulangerie.create({ name: 'Ô Fournil des Artistes', position: "45.777141, 4.833240", badge: "Argent", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
+Boulangerie.create({ name: 'Boulangerie Les Frangins', position: "45.762103, 4.836533", badge: "Bronze", prix_cr: 1, prix_pc: 1.2, time_open: Time.new(2002, 10, 31, 8, 0, 0), time_close: Time.new(2002, 10, 31, 18, 0, 0), image: "maisonjackq.jpg", email:"vincent.gonnard@gmail.com"})
 
-Boulangerie.create!(name: 'Eric Kayser', position: "45.768540,4.836213", badge: "Or", prix_cr: "4", prix_pc: "4", time_open: Time.new(2002, 10, 31, 8, 2, 2), time_close: Time.new(2002, 10, 31, 18, 2, 2), image: 'erickayser.jpg')
-Boulangerie.create({ name: 'Maison Jacquard', position: "45.768947,4.832261", badge: "Or", prix_cr: "4", prix_pc: "4", time_open: Time.new(2002, 10, 31, 8, 2, 2), time_close: Time.new(2002, 10, 31, 18, 2, 2), image: "maisonjackq.jpg"})
-Boulangerie.create({ name: 'O tao bom', position: "45.769913,4.836036", badge: "Argent", prix_cr: "4", prix_pc: "4", time_open: Time.new(2002, 10, 31, 8, 2, 2), time_close: Time.new(2002, 10, 31, 18, 2, 2), image: "maisonjackq.jpg"})
-Boulangerie.create({ name: 'Antoinette Pain & Brioche', position: "45.767865,4.830878", badge: "Argent", prix_cr: "4", prix_pc: "4", time_open: Time.new(2002, 10, 31, 8, 2, 2), time_close: Time.new(2002, 10, 31, 18, 2, 2), image: "maisonjackq.jpg"})
-Boulangerie.create({ name: 'Au fournil du 6ème', position: "45.769465,4.844340", badge: "Bronze", prix_cr: "4", prix_pc: "4", time_open: Time.new(2002, 10, 31, 8, 2, 2), time_close: Time.new(2002, 10, 31, 18, 2, 2), image: "maisonjackq.jpg"})
-
+p "Create Rating "
+Rating.create({ star: 2, boulangerie_id: 1, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 1, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 1, user_id: 1})
+Rating.create({ star: 1, boulangerie_id: 1, user_id: 1})
+Rating.create({ star: 2, boulangerie_id: 2, user_id: 1})
+Rating.create({ star: 4, boulangerie_id: 2, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 2, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 2, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 3, user_id: 1})
+Rating.create({ star: 2, boulangerie_id: 3, user_id: 1})
+Rating.create({ star: 1, boulangerie_id: 3, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 3, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 4, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 4, user_id: 1})
+Rating.create({ star: 4, boulangerie_id: 4, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 4, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 5, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 5, user_id: 1})
+Rating.create({ star: 2, boulangerie_id: 5, user_id: 1})
+Rating.create({ star: 1, boulangerie_id: 5, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 5, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 5, user_id: 1})
+Rating.create({ star: 2, boulangerie_id: 6, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 6, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 6, user_id: 1})
+Rating.create({ star: 1, boulangerie_id: 6, user_id: 1})
+Rating.create({ star: 2, boulangerie_id: 7, user_id: 1})
+Rating.create({ star: 4, boulangerie_id: 7, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 7, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 7, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 8, user_id: 1})
+Rating.create({ star: 2, boulangerie_id: 8, user_id: 1})
+Rating.create({ star: 1, boulangerie_id: 8, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 8, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 9, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 9, user_id: 1})
+Rating.create({ star: 4, boulangerie_id: 9, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 9, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 10, user_id: 1})
+Rating.create({ star: 3, boulangerie_id: 10, user_id: 1})
+Rating.create({ star: 2, boulangerie_id: 10, user_id: 1})
+Rating.create({ star: 1, boulangerie_id: 10, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 10, user_id: 1})
+Rating.create({ star: 5, boulangerie_id: 10, user_id: 1})
+p "Done"
 

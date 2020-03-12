bcd32e5f2c9d459f57f23982c13637dd5214b99b
Vincent Gonnard <vincent.gonnard@gmail.com>
2020-03-12 20:38:46 +0100

    fix bug heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 p "Deleting all the content "
-Boulangerie.destroy_all
 User.destroy_all
+Boulangerie.destroy_all
 Rating.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
